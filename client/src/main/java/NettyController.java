25e273a9b5a38d4383790ac37716c2d9c4103627
levinMK23 <levinmk23@gmail.com>
2020-08-04 21:25:23 +0300

    Lesson4

--- client/src/main/java/NettyController.java
+++ client/src/main/java/NettyController.java
@@ -1,3 +1,5 @@
+import io.netty.handler.codec.serialization.ObjectDecoderInputStream;
+import io.netty.handler.codec.serialization.ObjectEncoderOutputStream;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -6,7 +8,12 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.Socket;
 import java.net.URL;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.ResourceBundle;
@@ -17,13 +24,30 @@ public class NettyController implements Initializable {
     public ListView<String> listView;
     public TextField text;
     private NettyNetwork network;
+    private Socket socket;
+    private ObjectEncoderOutputStream os;
+    private ObjectDecoderInputStream is;
+
 
     public void sendCommand(ActionEvent actionEvent) {
-        network.sendMessage(Arrays.asList(text.getText()));
+        try {
+            Path path = Paths.get("./client/src/main/resources");
+            String fileName = text.getText();
+            os.writeObject(new File(path.toString() + "/" + fileName));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        network = NettyNetwork.getInstance();
+        try {
+            socket = new Socket("localhost", 8189);
+            os = new ObjectEncoderOutputStream(socket.getOutputStream());
+            is = new ObjectDecoderInputStream(socket.getInputStream());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 }
